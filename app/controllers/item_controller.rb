e345c008c41360365e384afe495ef8db37c0ba69
John Armstrong <john.armstrong@solnet.co.nz>
2016-03-01 20:14:36 +1300

    Added a create item funciton in the item controller

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,2 +1,24 @@
 class ItemController < ApplicationController
+
+  def create
+    if (List.exists? params[:list_id])
+      list = List.find params[:list_id]
+      item = Item.new(item_params)
+      list.items.push(item)
+
+      if list.save
+        render :json => {:message => "Item saved successfully"}
+      else
+        render :json => {:message => list.errors.messages.to_json}, :status => 400
+      end
+    else
+      render :json => {:message => "List not found"}, :status => 400
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name)
+  end
 end
