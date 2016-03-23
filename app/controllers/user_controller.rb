d20c4eafe081209d201a9e76affbf134426e2db2
John Armstrong <j.armstrong484@gmail.com>
2016-03-23 20:16:40 +1300

    Now returns an auth token when user logs in

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,14 @@
 class UserController < ApplicationController
-  before_action :auth_user, :except=> [:sign_up, :login]
+  before_action :auth_user, :except => [:sign_up, :login]
 
   def sign_up
     new_user = User.new(user_params)
 
     if new_user.save
-      render :json => {:message => "User created successfully"}, :status => 201
+      token = login_user new_user
+      response.headers["token"] = token
+
+      render :json => {:token => token}
     else
       render :json => {:message => new_user.errors.messages}, :status => 400
     end
@@ -19,13 +22,7 @@ class UserController < ApplicationController
     end
 
     if user.authenticate user_params[:password]
-
-      expiry = Time.now + 7.days
-
-      payload = {:username => user.username, :exp => expiry.to_i}
-
-      token = JWT.encode payload, ENV["SECRET"], 'HS256'
-
+      token = login_user user
       response.headers["token"] = token
 
       render :json => {:token => token}
@@ -38,5 +35,13 @@ class UserController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :password, :auth_token)
+  end
+
+  def login_user user
+    expiry = Time.now + 7.days
+
+    payload = {:username => user.username, :exp => expiry.to_i}
+
+    return JWT.encode payload, ENV["SECRET"], 'HS256'
   end
 end
