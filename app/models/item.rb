7a4610ced780fe3e72526fa756ac3e7fc561c069
John Armstrong <john.armstrong@solnet.co.nz>
2016-05-22 17:46:24 +1200

    Added model validation for order field for both list and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :list
   validates :name, presence: true
+  validates :order, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ActiveRecord::Base
   has_many :items, dependent: :destroy
   validates :name, presence: true
+  validates :order, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
 
   has_and_belongs_to_many :users
 end
