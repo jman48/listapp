d68ba852aa7cba4ddcf5d7151dcd8b1b31c98d0f
John Armstrong <john.armstrong@solnet.co.nz>
2016-05-22 19:17:39 +1200

    Updated route ordering to avoid incorrect route matches

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,17 @@ Rails.application.routes.draw do
     get '/' => 'list#all'
     get '/:id' => 'list#show'
     delete '/:id' => 'list#delete'
+    put '/order' => 'list#order'
     put '/:id' => 'list#update'
     post '/:id/users' => 'list#add_user'
-    put '/order' => 'list#order'
 
     scope '/:list_id/items' do
       post '/' => 'item#create'
       get '/' => 'item#list'
       delete '/:id' => 'item#delete'
       get '/:id' => 'item#show'
-      put '/:id' => 'item#update'
       put '/order' => 'item#order'
+      put '/:id' => 'item#update'
     end
   end
 
