57a7d50f45aaa04ee1c89f3cd7a5cc6ece0ac875
John Armstrong <john.armstrong@solnet.co.nz>
2016-05-23 19:19:20 +1200

    Added default callback to before validation to ensure it is always set

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
   belongs_to :list
   validates :name, presence: true
   validates :order, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
-  before_create :set_defaults
+  before_validation :set_defaults
 
   private
 
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ActiveRecord::Base
   has_many :items, dependent: :destroy
   validates :name, presence: true
   validates :order, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
-  before_create :set_defaults
+  before_validation :set_defaults
 
   has_and_belongs_to_many :users
 
