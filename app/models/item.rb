6553a4fd7813818e03faf22f3ac182973f715a95
John Armstrong <john.armstrong@solnet.co.nz>
2016-05-23 19:07:13 +1200

    Added callback to set order to 0 if user has not set it

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,13 @@ class Item < ActiveRecord::Base
   belongs_to :list
   validates :name, presence: true
   validates :order, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
+  before_save :set_defaults
+
+  private
+
+  def set_defaults
+    if !self.order
+      self.order = 0
+    end
+  end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,15 @@ class List < ActiveRecord::Base
   has_many :items, dependent: :destroy
   validates :name, presence: true
   validates :order, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
+  before_save :set_defaults
 
   has_and_belongs_to_many :users
+
+  private
+
+  def set_defaults
+    if !self.order
+      self.order = 0
+    end
+  end
 end
