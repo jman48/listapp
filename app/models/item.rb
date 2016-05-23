76808b60787588dbdd2aad640ed1a028c0b7b6f8
John Armstrong <john.armstrong@solnet.co.nz>
2016-05-23 19:09:51 +1200

    Added callback to set order to 0 if user has not set it before item is created

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
   belongs_to :list
   validates :name, presence: true
   validates :order, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
-  before_save :set_defaults
+  before_create :set_defaults
 
   private
 
