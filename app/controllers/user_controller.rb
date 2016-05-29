56ecdc9f0bbb37b54ee981de174816703f8a4b1a
John Armstrong <john.armstrong@solnet.co.nz>
2016-05-29 19:51:42 +1200

    Now returns just the users username

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
       render :json => {:message => "Search string is to short. Minimum of 3"}, :status => 400 and return false
     end
 
-    users = User.where("username LIKE ?", search_string + "%")
+    users = User.where("username LIKE ?", search_string + "%").pluck(:username)
 
     render :json => users.to_json, :status => 200
   end
