947414979857c22d9e02df9c765d67b1653faff0
John Armstrong <john.armstrong@solnet.co.nz>
2016-05-29 15:40:43 +1200

    Added a can_access function for list and items so that it is easier to determine if a user has access to them

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,13 @@ class Item < ActiveRecord::Base
   validates :order, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
   before_validation :set_defaults
 
+  #Check whether a user can access this item
+  def can_access user
+    list = List.find(self.list_id)
+
+    return list.can_access user
+  end
+
   private
 
   def set_defaults
