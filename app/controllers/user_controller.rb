6213a130435180ababfe6b73129de81f7cdf325d
John Armstrong <j.armstrong484@gmail.com>
2016-06-15 18:31:18 +1200

    Now compares search as lower case when searching for users by lower case

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
       render :json => {:message => "Search string is to short. Minimum of 3"}, :status => 400 and return false
     end
 
-    users = User.where("username LIKE ?", search_string + "%").pluck(:username, :picture)
+    users = User.where("username LIKE ?", search_string.downcase + "%").pluck(:username, :picture)
 
     users = users.map {|user|
       {:username => user[0], :picture => user[1]}
